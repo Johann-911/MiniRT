ea92a5a63cbe74182a6de85320458767b6d8e5a9
Janni <janni.toumanidis@web.de>
2026-02-18 15:12:55 +0100

    first try of an window mlx

---
+++ inc/window.h
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../minilibx-linux/mlx.h"
+#include <X11/Xlib.h>
+
+
+typedef struct s_app 
+{
+    void *mlx;
+    void *win;
+    void *img;
+    int width;
+    int height;
+}	t_app;
+
+# define HEIGHT 500
+# define WIDTH 500
+#define KEY_ESC 65307
+
+void	put_pixel(char *data, int x, int y, int color, int bpp, int line);
+void	fill_gradient(t_app *app);
+int	handle_key(int keycode, void *param);
+int	handle_mouse(int button, int x, int y, void *param);
+int	handle_close(void *param);
+void recreate_image_and_draw(t_app *app);
+
---
+++ main/main.c
@@ -0,0 +1,124 @@
+#include "../inc/window.h"
+
+
+void put_pixel(char *data, int x, int y, int color, int bpp, int line)
+{
+    int offset;
+    offset = y * line + x * (bpp / 8);
+    *(int *)(data + offset) = color;
+}
+
+void draw_image(t_app *app)
+{
+    int x;
+    int y;
+    int bpp;
+    int line;
+    int endian;
+    char *data;
+
+    data = mlx_get_data_addr(app->img, &bpp, &line, &endian);
+    y = 0;
+    while (y < app->height)
+    {
+        x = 0;
+        while (x < app->width)
+        {
+            int r;
+            int g;
+            int b;
+            int color;
+
+            r = (x * 255) / app->width;
+            g = (y * 255) / app->height;
+            b = 128;
+            color = (r << 16) | (g << 8) | b;
+            put_pixel(data, x, y, color, bpp, line);
+            x++;
+        }
+        y++;
+    }
+}
+   
+void	fill_gradient(t_app *app)
+{
+    int x, y;
+    int bpp;
+	int line;
+	int  endian;
+    char *data = mlx_get_data_addr(app->img, &bpp, &line, &endian);
+    y = 0;
+    while (y < app->height)
+    {
+        x = 0;
+        while (x < app->width)
+        {
+            int r = (x * 255) / app->width;
+            int g = (y * 255) / app->height;
+            int b = 128;
+            int color = (r << 16) | (g << 8) | b;
+            put_pixel(data, x, y, color, bpp, line);
+            x++;
+        }
+        y++;
+    }
+    mlx_put_image_to_window(app->mlx, app->win, app->img, 0, 0);
+}
+
+/* key handler (KeyPress) */
+int	handle_key(int keycode, void *param)
+{
+    (void)param;
+    if (keycode == KEY_ESC)
+        exit(0);
+    return (0);
+}
+
+/* window close (red cross) */
+int	handle_close(void *param)
+{
+    (void)param;
+    exit(0);
+    return (0);
+}
+
+int handle_mouse(int button, int x, int y, void *param)
+{
+    (void)x; (void)y; (void)param;
+    if (button == 4)
+        /* scroll up */ ;
+    else if (button == 5)
+		return (0);
+}
+
+int	main(void)
+{
+    t_app app;
+
+    app.width = 800;
+    app.height = 600;
+    app.mlx = mlx_init();
+    if (!app.mlx)
+    {
+        perror("mlx_init");
+        return (1); 
+    }
+    app.win = mlx_new_window(app.mlx, app.width, app.height, "miniRT-MLX test");
+    if (!app.win)
+    {
+        perror("mlx_new_window");
+        return (1);
+    }
+    app.img = mlx_new_image(app.mlx, app.width, app.height);
+    if (!app.img)
+    {
+        perror("mlx_new_image");
+        return (1);
+    }
+    fill_gradient(&app);
+    mlx_hook(app.win, 2, 1L<<0, handle_key, &app);
+    mlx_hook(app.win, 17, 0, handle_close, &app);
+    mlx_mouse_hook(app.win, handle_mouse, &app);
+    mlx_loop(app.mlx);
+    return (0);
+}
